b38e4b5c6487362d805516ba69a42bbc1446997d
GeekTR <geektheripper@gmail.com>
2018-11-24 07:41:52 +0800

    fix bugs

--- dump-webgui.sh
+++ dump-webgui.sh
@@ -3,10 +3,12 @@
 rm -rf webgui
 mkdir -p webgui/public
 
+cp conf/ssmgr/skin/laughing-man-rotate.svg webgui
+
 docker run --rm \
   --volume "$PWD/webgui:/tmp/webgui" \
   --user "$UID" \
-  --workdir /usr/lib/node_modules/shadowsocks-manager/plugins/webgui \
+  --workdir /usr/local/share/.config/yarn/global/node_modules/shadowsocks-manager/plugins/webgui \
   -it \
-  gyteng/ssmgr /bin/bash -c \
+  geektr/ssmgr /bin/bash -c \
   "cp -r libs /tmp/webgui; cp -r public/views /tmp/webgui/public"
